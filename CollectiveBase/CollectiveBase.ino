6456312245dfb6d8f99d4fefd0618e978e8e7dd5
Ryan Spicer <ryanspicer@gmail.com>
2018-02-28 07:56:41 -0800

    Added readme and Arduino sketches.

---
+++ CollectiveBase/CollectiveBase.ino
@@ -0,0 +1,87 @@
+/**************************************************************************/
+/*!
+    @file     angle_moving_avg.ino
+    @author   SOSAndroid (E. Ha.)
+    @license  BSD (see license.txt)
+
+  read over I2C bus and averaging angle
+
+    @section  HISTORY
+
+    v1.0 - First release
+*/
+/**************************************************************************/
+
+#include <ams_as5048b.h>
+
+//unit consts
+#define U_DEG 3
+#undef EXP_MOVAVG_N
+#define EXP_MOVAVG_N 20
+
+AMS_AS5048B mysensor;
+
+
+const double angleMin = 162.0;
+const double angleMax = 208.0;
+
+int count = 0;
+
+
+void setup() {
+
+  //Start serial
+  Serial.begin(9600);
+  while (!Serial) ; //wait until Serial ready
+
+  //Start Wire object. Unneeded here as this is done (optionnaly) by AMS_AS5048B object (see lib code - #define USE_WIREBEGIN_ENABLED)
+  //Wire.begin();
+
+  //init AMS_AS5048B object
+  mysensor.begin();
+
+  //set clock wise counting
+  mysensor.setClockWise(false); 
+
+  //set the 0 to the sensorr
+  //mysensor.zeroRegW(0x0);
+
+}
+
+
+void loop() {
+  
+  //prints to serial the read angle in degree and its average every 2 seconds
+  //prints 2 times the exact same angle - only one measurement
+  mysensor.updateMovingAvgExp();
+
+
+  //Serial.print("Angle degree : ");
+  //Serial.println(mysensor.angleR(U_DEG, false), DEC);
+
+  //Serial.print("Average ");
+  //Serial.println(mysensor.getMovingAvgExp(U_DEG), DEC);
+
+  //double angle = mysensor.angleR(U_DEG, false);
+  double angle = mysensor.getMovingAvgExp(U_DEG);;
+  double scaled = (angle - angleMin) / (angleMax - angleMin);
+
+  if (scaled > 1.0) scaled = 1.0;
+  if (scaled < 0.0) scaled = 0.0;
+
+  uint16_t tenBit = (uint16_t)(scaled * 1024.0);
+  
+  Joystick.X(tenBit);
+
+  count++;
+  if (count % 15 == 0)
+  {
+    Serial.print(angle);
+    Serial.print(" ");
+    Serial.print(scaled);
+    Serial.print(" ");
+    Serial.print(tenBit);
+    Serial.println();
+  }
+  delay(1);
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# SimulatorCollectiveFirmware
+# Simulator Collective Firmware
+
+This repo contains firmware for a DIY simulator collective. The intent is not to commercialize, but rather to design something usable that an interested hobbyist with a relatively limited fabrication setup (ie: yours truly) can build and customize. Because I'm an aviation simulation enthusiast who likes physically correct (or as close as I can get for affordable prices) switchology, the "head" or "button box" component is designed to be interchangeable to support different airframes' controls. So far there are two software components:
+
+## Collective Base
+
+This firmware is intended to run on a Teensy 3.x, with the Joystick USB profile. It uses a contactless hall effect sensor to measure the collective arm's angle, and integrates input from the interchangeable button box. In future hardware/software revisions I hope to connect a twist throttle and collective brake as well.
+
+## Collective Button Box
+
+This firmware interfaces switches with the Base as an I2C slave. I probably could've used 74HC165 shift registers, like the Thrustmaster Cougar/Warthog, and just put all the brains in the Collective Base, but I like the idea of being able to support joystick axes (eg: for the spotlight slew control on some DCS helicopters) and for the box to be able to report things like its intended range of motion to the base.
